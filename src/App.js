f0c34527c2d034dab092ea3d34716db71fc0c99b
VardaPanchal005 <114830938+VardaPanchal005@users.noreply.github.com>
2024-12-31 14:03:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,104 +4,29 @@ const json = {
   "logoPosition": "right",
   "pages": [
     {
-      "name": "page1",
+      "name": "personalInfo",
       "elements": [
-        {
-          "type": "text",
-          "name": "question1",
-          "title": "First Name",
-          "isRequired": true
-        },
-        {
-          "type": "text",
-          "name": "question3",
-          "title": "Last Name",
-          "isRequired": true
-        },
-        {
-          "type": "text",
-          "name": "question2",
-          "title": "Email",
-          "isRequired": true,
-          "inputType": "email"
-        },
-        {
-          "type": "text",
-          "name": "question4",
-          "title": "Phone",
-          "inputType": "tel"
-        }
+        { "type": "text", "name": "firstName", "title": "First Name", "isRequired": true },
+        { "type": "text", "name": "lastName", "title": "Last Name", "isRequired": true },
+        { "type": "text", "name": "email", "title": "Email", "isRequired": true, "inputType": "email" },
+        { "type": "text", "name": "phone", "title": "Phone", "inputType": "tel" }
       ]
     },
     {
-      "name": "page2",
+      "name": "carOwnership",
       "elements": [
-        {
-          "type": "radiogroup",
-          "name": "question5",
-          "title": "Do you own a car?",
-          "choices": [
-            { "value": "Item 1", "text": "Yes" },
-            { "value": "Item 2", "text": "No" }
-          ]
-        },
-        {
-          "type": "dropdown",
-          "name": "question6",
-          "visibleIf": "{question5} = 'Item 1'",
-          "title": "What car brand do you own?",
-          "choices": [
-            { "value": "Item 1", "text": "Toyota" },
-            { "value": "Item 2", "text": "BMW" },
-            { "value": "Item 3", "text": "Honda" }
-          ]
-        },
-        {
-          "type": "text",
-          "name": "question7",
-          "visibleIf": "{question5} = 'Item 1'",
-          "title": "How many kilometers do you drive per month?",
-          "inputType": "number"
-        },
-        {
-          "type": "radiogroup",
-          "name": "question8",
-          "visibleIf": "{question5} = 'Item 1'",
-          "title": "Do you travel frequently for work?",
-          "choices": [
-            { "value": "Item 1", "text": "Yes" },
-            { "value": "Item 2", "text": "No" }
-          ]
-        },
-        {
-          "type": "checkbox",
-          "name": "question9",
-          "visibleIf": "{question8} = 'Item 1'",
-          "title": "Which mode of transportation do you prefer?",
-          "choices": [
-            { "value": "Item 1", "text": "Flight" },
-            { "value": "Item 2", "text": "Train" },
-            { "value": "Item 3", "text": "Car" },
-            { "value": "Item 4", "text": "Bus" }
-          ]
-        }
+        { "type": "radiogroup", "name": "ownsCar", "title": "Do you own a car?", "choices": [{ "value": "Yes", "text": "Yes" }, { "value": "No", "text": "No" }] },
+        { "type": "dropdown", "name": "carBrand", "visibleIf": "{ownsCar} = 'Yes'", "title": "What car brand do you own?", "choices": [{ "value": "Toyota", "text": "Toyota" }, { "value": "BMW", "text": "BMW" }, { "value": "Honda", "text": "Honda" }] },
+        { "type": "text", "name": "kilometersDriven", "visibleIf": "{ownsCar} = 'Yes'", "title": "How many kilometers do you drive per month?", "inputType": "number" },
+        { "type": "radiogroup", "name": "travelFrequently", "visibleIf": "{ownsCar} = 'Yes'", "title": "Do you travel frequently for work?", "choices": [{ "value": "Yes", "text": "Yes" }, { "value": "No", "text": "No" }] },
+        { "type": "checkbox", "name": "preferredTransportation", "visibleIf": "{travelFrequently} = 'Yes'", "title": "Which mode of transportation do you prefer?", "choices": [{ "value": "Flight", "text": "Flight" }, { "value": "Train", "text": "Train" }, { "value": "Car", "text": "Car" }, { "value": "Bus", "text": "Bus" }] }
       ]
     },
     {
-      "name": "page3",
+      "name": "surveyFeedback",
       "elements": [
-        {
-          "type": "rating",
-          "name": "question10",
-          "title": "The survey was easy to understand",
-          "isRequired": true
-        },
-        {
-          "type": "rating",
-          "name": "question12",
-          "title": "The survey was easy to understand",
-          "isRequired": true
-        }
+        { "type": "rating", "name": "surveyEasyRating", "title": "The survey was easy to understand", "isRequired": true, "rateType": "stars" },
+        { "type": "rating", "name": "surveyClearRating", "title": "The instructions were clear", "isRequired": true, "rateType": "stars" }
       ]
     }
   ]
@@ -111,15 +36,47 @@ function App() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const survey = new window.Survey.Model(json);
-      survey.onComplete.add((sender, options) => {
-        console.log(JSON.stringify(sender.data, null, 3));
+
+      survey.onComplete.add((sender) => {
+        const surveyData = sender.data;
+
+        // Use jQuery AJAX call to submit data
+        sendSurveyData(surveyData);
       });
+
       window.$("#surveyElement").Survey({ model: survey });
     }
   }, []);
 
+  const sendSurveyData = (data) => {
+    window.$.ajax({
+      url: "https://apim.quickwork.co/employeelogin/mypath/version/assignedpath",
+      data: JSON.stringify(data),
+      contentType: 'application/json;charset=utf-8',
+      type: "POST",
+      headers: { 'ApiKey': "JjUNK0e7DGwu2A9rTjvJWZ3YHcxdKrE4" },
+      success: function() {
+        alert('Survey submitted successfully!');
+      },
+      error: function() {
+        alert('Failed to submit survey. Please try again.');
+      }
+    });
+  };
+
   return (
-    <div id="surveyElement" style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, minHeight: '100%', height: '100%' }} />
+    <div
+      id="surveyElement"
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        minHeight: '100%',
+        height: '100%',
+      }}
+    />
   );
 }
 
